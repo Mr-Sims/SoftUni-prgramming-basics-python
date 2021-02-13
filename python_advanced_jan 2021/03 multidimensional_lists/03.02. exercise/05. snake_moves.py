9ed6a8c4f6b64c90a4fe8c4a40769d733fbbbb66
Simeon Rangelov <67736865+Mr-Sims@users.noreply.github.com>
2021-02-13 10:43:02 +0200

    Update 05. snake_moves.py

--- python_advanced_jan 2021/03 multidimensional_lists/03.02. exercise/05. snake_moves.py
+++ python_advanced_jan 2021/03 multidimensional_lists/03.02. exercise/05. snake_moves.py
@@ -33,28 +33,45 @@ print_matrix(matrix)
 ###########################################################
 ### v2 - no funcs, using only lists and list slicing
 
-# rows, cols = [int(el) for el in input().split()]
-# word = input()
-# count = 0
-# a_list = []
-#
-# for _ in range(rows * cols):
-#     if count == len(word):
-#         count = 0
-#     a_list.append(word[count])
-#     count += 1
-#
-# a_list = ''.join(a_list)
-# start, stop = 0, cols
-#
-# for i in range(rows):
-#     if i % 2 != 0:
-#         print(a_list[stop - 1:start - 1:-1])
-#     else:
-#         print(a_list[start:stop])
-#     start += cols
-#     stop += cols
+rows, cols = [int(el) for el in input().split()]
+word = input()
+count = 0
+a_list = []
+
+for _ in range(rows * cols):
+    if count == len(word):
+        count = 0
+    a_list.append(word[count])
+    count += 1
+
+a_list = ''.join(a_list)
+start, stop = 0, cols
+
+for i in range(rows):
+    if i % 2 != 0:
+        print(a_list[stop - 1:start - 1:-1])
+    else:
+        print(a_list[start:stop])
+    start += cols
+    stop += cols
 
 ###########################################################
 ###########################################################
-### v3
+### v3 no funcs but with some killer snake moves!!!
+
+from collections import deque
+rows, cols = input().split()
+line = deque(list(input()))
+matrix = [[0 for el in range(int(cols))] for row in range(int(rows))]
+
+for row_index in range(int(rows)):
+    if row_index % 2 == 0:
+        for col_index in range(int(cols)):
+            matrix[row_index][col_index] = line[0]
+            line.rotate(-1)
+        print(*matrix[row_index], sep="")
+    else:
+        for col_index in range(int(cols)-1, -1, -1):
+            matrix[row_index][col_index] = line[0]
+            line.rotate(-1)
+        print(*matrix[row_index], sep="")
