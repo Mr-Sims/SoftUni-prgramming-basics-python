545e100efd6cd70466088d7ff0133a994f7494fc
Simeon Rangelov <67736865+Mr-Sims@users.noreply.github.com>
2021-03-23 22:11:56 +0200

    school problems

---
+++ python_advanced_jan 2021/python_OOP/lesson_05_inheritance/exercise/ex_04_need_for_speed/project/family_car.py
@@ -0,0 +1,7 @@
+from project.car import Car
+
+
+class FamilyCar(Car):
+    def __init__(self, fuel: float, horse_power: int):
+        super().__init__(fuel, horse_power)
+        self.fuel_consumption = float(Car.DEFAULT_FUEL_CONSUMPTION)
